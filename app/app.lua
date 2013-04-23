0c728c3f2767b6f76cc3c350a87eec9eb6804ec4
Jack Lawson <ajacksified@gmail.com>
2013-04-22 21:39:22 -0700

    updated lusty, much cleaner config

--- app/app.lua
+++ app/app.lua
@@ -10,60 +10,53 @@ local config = {
   server = require 'lusty-nginx.server',
 
   subscribers = {
-    input = {
-      -- decode json input if it exists in the body data
-      { ['lusty-json.input.json'] = { json = require 'cjson' } }
+    ['input'] = {
+      -- decode json input if it exists in the body data.
+      -- you can provide -- options to the handler as a table.
+      -- in this case, we are passing in a json encoding/decoding function.
+      ['lusty-json.input.json'] = { json = require 'cjson' }
     },
 
-    request = {
-      -- / is routed to /index in nginx
-      -- the ./handlers/index.lua file is loaded when /index is requested
-      index = 'handlers.requests.index',
+    -- / is routed to /index in nginx
+    -- ./handlers/requests/index.lua is loaded when /index is requested
+    ['request:index'] = { 'handlers.requests.index' },
+    ['request:404'] = { 'handlers.requests.404' },
+    ['request:500'] = { 'handlers.requests.500' },
 
-      -- 404 route and catch-all to redirect to 404
-      -- the ./handlers/404.lua file is loaded when /404 is requested
-      ["404"] = 'handlers.requests.404',
-      ["500"] = 'handlers.requests.500',
+    -- default to 404 if none of the above request routes match
+    ['request'] = { 'handlers.requests.404' },
 
-      -- the ./handlers/404.lua file is loaded if the request didn't match any
-      -- above routes (an entry with no key is a default, "root" handler)
-      'handlers.requests.404'
+    -- add mustache parsing to the render events
+    ['render'] = { 'lusty-mustache.render.mustache' },
+
+    -- capture html requests as mustache handlers, and
+    -- capture json requests to output handler data as json
+    ['output'] = {
+      'lusty-html.output.html',
+      ['lusty-json.output.json'] = { json = require 'cjson' }
     },
 
-    render = {
-      { ['lusty-mustache.render.mustache'] = {} }
-    },
-
-    output = {
-      -- capture html requests as mustache handlers
-      { ['lusty-html.output.html'] = { } },
-
-      -- capture json requests to output handler data as json
-      { ['lusty-json.output.json'] = { json = require 'cjson' } }
-    },
-
-    log = {
-      -- log events should write to the console
-      { ['lusty-log-console.log.console'] = {} },
-
-      -- log events should also go up to nginx
-      { ['lusty-nginx.log'] = {} },
+    -- log events should write to the console
+    -- log events should also go up to nginx
+    ['log'] = {
+      'lusty-log-console.log.console',
+      'lusty-nginx.log'
     }
   },
 
   -- as requests come in, fire these events in order (corresponding to
   -- subscribers above)
   publishers = {
-    {"input"},
-    {"request"},
-    {"render"},
-    {"output"}
+    {'input'},
+    {'request'},
+    {'render'},
+    {'output'}
   },
 
   -- bind context methods to the context object that is passed around, so you
   -- can use things like context.log and context.store from within your handler
   context = {
-    ['lusty-log.context.log'] = { level = "debug" }
+    ['lusty-log.context.log'] = { level = 'debug' }
   }
 }
 
